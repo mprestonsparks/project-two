e71edc27861a14764acd48384780146b37d70629
jstevens79 <jstevens79@users.noreply.github.com>
2019-05-02 20:10:39 -0500

    getting started adding handlebars templates

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,49 @@
-var db = require("../models");
+//var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
+
+  // // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
+
+  app.get('/', (req, res) => {
+    res.render("index");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
+  app.get('/dashboard', (req, res) => {
+    res.render("dashboard")
+  })
+
+  app.get('/project', (req, res) => {
+    res.render("project")
+  })
+
+  app.get('/projects', (req, res) => {
+    res.render("projects");
+  })
+
+  app.get('/users', (req, res) => {
+    res.render("users")
+  })
+
+
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", (req, res) => {
     res.render("404");
   });
 };
