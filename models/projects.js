ba12a97ae7db3387f6e8216ab49c049556bd1d6a
Luke Duran <ltduran.lsp@gmail.com>
2019-05-04 11:17:27 -0500

    made changes to the project-project lead association

--- models/projects.js
+++ models/projects.js
@@ -1,25 +1,18 @@
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Project = sequelize.define("Project", {
         // Giving the Author model a name of type STRING
         project_name: {
             type: Sequelize.STRING,
             allowNull: false
         },
-        project_description: DataTypes.TEXT,
-        project_lead: {
-            type: Sequelize.STRING,
-            references: 'users', // References 'users' table
-            referencesKey: 'user_id'
-        }
+        project_description: DataTypes.TEXT
     });
-    
-    Project.associate = function(models) {
+
+    Project.associate = function (models) {
         // Associating Projects with Users
-        Project.hasMany(models.Users, {
-        // onDelete: "cascade"
-        });
+        Project.belongsTo(models.Users);
     };
-    
+
     return Project;
 };
