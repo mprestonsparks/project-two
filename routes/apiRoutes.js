4e429010cbad691310f6feb43037553c9a11b859
Cody <cody.tracy92@gmail.com>
2019-05-04 16:49:37 -0500

    fixed some of the api errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,19 +4,27 @@ module.exports = function(app) {
   // Need to update the database/models for this section
 
   app.get("/api/users", function(req,res) {
-    db.User.findAll({}).then(function(data){
+    db.User.findAll({
+      attributes: ["user_id","user_first_name","user_last_name","user_email"]
+    }).then(function(data){
       res.json(data);
     });
   });
 
   app.get("/api/users/:user_id",function(req,res){
-    db.User.findAll({where: {user_id: req.params.user_id} }).then(function(data){
+    db.User.findAll({
+      where: {user_id: req.params.user_id},
+      attributes: ["user_id","user_first_name","user_last_name","user_email"], 
+    }).then(function(data){
       res.json(data);
     });
   });
 
   app.get("/api/users/:project_name",function(req,res){
-    db.User.findAll({where: {project_name: req.params.project_name} }).then(function(data){
+    db.User.findAll({
+      where: {project_name: req.params.project_name},
+      attributes: ["user_id","user_first_name","user_last_name","user_email"]
+     }).then(function(data){
       res.json(data);
     });
   });
@@ -31,31 +39,42 @@ module.exports = function(app) {
   }); 
 
   app.get("/api/projects/:project_id",function(req,res){
-    db.Project.findAll({where: {project_id: req.params.project_id} }).then(function(data){
+    db.Project.findAll({
+      where: {project_id: req.params.project_id},
+      attributes: ["id","project_name","project_description","project_lead"]
+     }).then(function(data){
       res.json(data);
     });
   });
 
   app.get("/api/projects/:user_id",function(req,res){
-    db.Project.findAll({where: {user_id: req.params.user_id} }).then(function(data){
+    db.Project.findAll({
+      where: {user_id: req.params.user_id},
+      attributes: ["id","project_name","project_description","project_lead"]
+     }).then(function(data){
       res.json(data);
     });
   });
 
   app.get("/api/tasks/",function(req,res){
-    db.Example.findAll({}).then(function(data){
+    db.Task.findAll({
+      attributes: ["task_id", "task_name", "task_description", "goal_start", "goal_end", "actual_start", "actual_end"]
+    }).then(function(data){
       res.json(data);
     });
   });
 
   app.get("/api/tasks/:project_id",function(req,res){
-    db.Example.findAll({where: {project_id: req.params.project_id} }).then(function(data){
+    db.Task.findAll({
+      where: {project_id: req.params.project_id},
+      attributes: ["task_id", "task_name", "task_description", "goal_start", "goal_end", "actual_start", "actual_end"]
+    }).then(function(data){
       res.json(data);
     });
   });
 
   app.get("/api/tasks/:user_id",function(req,res){
-    db.Example.findAll({where: {use_idr: req.params.user_id} }).then(function(data){
+    db.Task.findAll({where: {use_idr: req.params.user_id} }).then(function(data){
       res.json(data);
     });
   });
@@ -67,19 +86,19 @@ module.exports = function(app) {
   });
 
   app.post("/api/task", function(req, res) {
-    db.Example.create(req.body).then(function(data) {
+    db.Task.create(req.body).then(function(data) {
       res.json(data);
     });
   });
 
   app.post("/api/user", function(req, res) {
-    db.Example.create(req.body).then(function(data) {
+    db.User.create(req.body).then(function(data) {
       res.json(data);
     });
   });
 
   app.delete("/api/task/:task_id", function(req, res) {
-    db.Example.destroy({ where: { task_id: req.params.task_id } }).then(function(data) {
+    db.Task.destroy({ where: { task_id: req.params.task_id } }).then(function(data) {
       res.json(data);
     });
   });
@@ -97,7 +116,7 @@ module.exports = function(app) {
   });
 
   app.delete("api/task/comment/:comment_id",function(req,res){
-    db.Example.destroy({where : { comment_id: req.params.comment_id} }).then(function(data){
+    db.Comment.destroy({where : { comment_id: req.params.comment_id} }).then(function(data){
       res.json(data);
     });
   });
@@ -115,7 +134,7 @@ module.exports = function(app) {
   });
 
   app.put("/api/task/:task_id", function(req, res) {
-    db.Example.update({ where: { task_id: req.params.task_id } }).then(function(data) {
+    db.Task.update({ where: { task_id: req.params.task_id } }).then(function(data) {
       res.json(data);
     });
   });
